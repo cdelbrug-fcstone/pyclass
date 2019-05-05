76a8dec90a2294c8888c23c532e5818bce411da8
Caleb Delbrugge <caleb.delbrugge@intlfcstone.com>
2019-05-05 08:40:32 -0700

    lesson 2 timing script - delay factors

---
+++ les2_showlldp_timing.py
@@ -0,0 +1,49 @@
+from netmiko import ConnectHandler
+from datetime import datetime
+
+
+nxos2 = {
+
+    "host": "nxos2.lasthop.io",
+    "username": "pyclass",
+    "password": "88newclass",
+    "device_type": "cisco_nxos",
+    "global_delay_factor": 2,
+}
+
+connection = ConnectHandler(**nxos2)
+
+time_before1 = datetime.now()
+output = connection.send_command("show lldp neighbors detail", strip_prompt=False, strip_command=False)
+time_after1 = datetime.now()
+
+print(f"""
+
+===========================================
+          GLOBAL DELAY FACTOR
+===========================================
+{output}
+===========================================
+    """)
+print(f"""
+-----------------------------------------
+     Time difference: {time_after1 - time_before1}
+-----------------------------------------
+    """)
+
+time_before2 = datetime.now()
+output2 = connection.send_command("show lldp neighbors detail", strip_prompt=False, strip_command=False, delay_factor=8)
+time_after2 = datetime.now()
+print(f"""
+
+===========================================
+          LOCAL DELAY FACTOR
+===========================================
+{output2}
+===========================================
+    """)
+print(f"""
+-------------------------------------------
+     Time: {time_after2 - time_before2}
+-------------------------------------------
+    """)
