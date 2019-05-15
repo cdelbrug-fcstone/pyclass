dbc9ff9ff8d39f3ea3fc46d87b8de46656b50e60
Caleb Delbrugge <caleb.delbrugge@intlfcstone.com>
2019-05-14 19:17:19 -0700

    final les2 file

---
+++ les2_final.py
@@ -0,0 +1,34 @@
+from netmiko import ConnectHandler
+import time
+
+cisco4 = {
+
+    "host": "cisco4.lasthop.io",
+    "username": "pyclass",
+    "password": "88newclass",
+    "device_type": "cisco_ios",
+    "secret": "88newclass",
+    "session_log": "my_output.txt",
+}
+
+connection = ConnectHandler(**cisco4)
+
+print(connection.find_prompt())
+
+connection.config_mode()
+
+print(connection.find_prompt())
+
+connection.exit_config_mode()
+
+print(connection.find_prompt())
+
+connection.write_channel("disable\n")
+time.sleep(2)
+
+print(connection.read_channel())
+
+connection.enable()
+
+print(connection.find_prompt())
+
